3a8edbc3a7d902af848577223527eea2cb15bb5d
Shrey Pandey <shreypandey27@gmail.com>
2022-03-23 02:05:39 +0530

    landing page updated

--- src/frontend/pages/landing/index.js
+++ src/frontend/pages/landing/index.js
@@ -2,8 +2,16 @@ import './landing.css';
 import Header from './Header';
 import Category from './Category';
 import { Footer } from '../../components';
+import { useLandingCtx } from '../../context';
+import { useEffect } from 'react';
 
 export default function Landing() {
+  const { dispatch } = useLandingCtx();
+  
+  useEffect(() => {
+    dispatch({ type: 'SET_FILTER', payload: 'All' });
+  }, [dispatch]);
+
   return (
     <div>
       <Header />
