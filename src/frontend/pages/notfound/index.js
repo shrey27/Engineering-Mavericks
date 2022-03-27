944f92893463593537275904dfcd0f91c02ae408
Shrey Pandey <shreypandey27@gmail.com>
2022-03-27 19:53:36 +0530

    not found page added

--- src/frontend/pages/notfound/index.js
+++ src/frontend/pages/notfound/index.js
@@ -2,14 +2,17 @@ import './notfound.css';
 import { Link } from 'react-router-dom';
 import { Navbar, Footer } from '../../components';
 import { LANDING } from '../../routes/routes';
+import pic from '../../assets/404.webp';
 
 export default function NotFound() {
   return (
     <div>
       <Navbar />
       <div className='notfound'>
-        <Link to={LANDING} className='btn btn--auth--solid'>
+        <img src={pic} alt='404' className='notfound__banner' />
+        <Link to={LANDING} className='btn btn--auth--solid md sb mg--full'>
           Start learning
+          <i className='fa-solid fa-right-to-bracket'></i>
         </Link>
       </div>
       <Footer fixed={true} />
