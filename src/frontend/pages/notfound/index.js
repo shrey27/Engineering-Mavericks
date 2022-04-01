1f6f5c3ccd58b65062c8fea6404bd62cfc920129
Shrey Pandey <shreypandey27@gmail.com>
2022-04-01 18:39:47 +0530

    404 page updated

--- src/frontend/pages/notfound/index.js
+++ src/frontend/pages/notfound/index.js
@@ -2,16 +2,17 @@ import './notfound.css';
 import { Link } from 'react-router-dom';
 import { Navbar, Footer } from '../../components';
 import { LANDING } from '../../routes/routes';
-import pic from '../../assets/404.webp';
+import pic from '../../assets/notfound.gif';
 
 export default function NotFound() {
   return (
     <div>
       <Navbar />
       <div className='notfound'>
+        <h1 className='notfound__primary'>404 Error Found!</h1>
         <img src={pic} alt='404' className='notfound__banner' />
         <Link to={LANDING} className='btn btn--auth--solid md sb mg--full'>
-          Start learning
+          Go back to Homepage
           <i className='fa-solid fa-right-to-bracket'></i>
         </Link>
       </div>
